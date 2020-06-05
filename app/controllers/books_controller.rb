afb7ab7efcc13dad6446a45dcec6e11caf974a39
katayama623 <k6t2y3m@gmail.com>
2020-06-05 13:51:15 +0000

    [add] モデルとコントローラーを追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,11 @@
+class BooksController < ApplicationController
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#home'
+  resources :users,only: [:show, :edit, :update, :index]
+  resources :posts, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resource :books, only: [:create, :destroy]
+  end
 end
