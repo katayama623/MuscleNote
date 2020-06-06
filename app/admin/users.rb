3776d2141d1176ca52d500ff98b3bf3ce931c6ae
katayama623 <k6t2y3m@gmail.com>
2020-06-06 01:46:46 +0000

    [fix] user一覧に投稿数を追加

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -14,5 +14,19 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  permit_params :name, :profile_image, :introduction
+  permit_params :name, :profile_image_id, :introduction
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :introduction
+    column '投稿数' do |user|
+      user.posts.count
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
