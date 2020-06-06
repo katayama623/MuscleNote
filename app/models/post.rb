23a5ab291068b10bbfe8ef67fb40697773036adc
katayama623 <k6t2y3m@gmail.com>
2020-06-06 11:30:24 +0000

    [add] Postにenumを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  has_many :post_comments
+  has_many :favorite
+  has_many :books
+
+  enum part: [:part0, :part1, :part2, :part3, :part4, :part5, :part6, :part7, :part8]
+  # 0=胸 1=背中 2=肩 3=腕 4=腹筋 5=太腿 6=上半身全体 7=下半身全体 8=その他
+
+  enum strong: [:strong0, :strong1, :strong2]
+  # 0=軽め 1=普通 2=強め
 end
