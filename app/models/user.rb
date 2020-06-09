4f25d5ef39798cdd8ef6b7aebc937197688f7f42
katayama623 <k6t2y3m@gmail.com>
2020-06-09 10:33:47 +0000

    [add] フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,27 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :booked_posts, through: :books, source: :post
 
+  # follow機能
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  #フォローしているかを確認するメソッド
+  def following?(user)
+    following_relationships.find_by(following_id: user.id)
+  end
+
+  #フォローするときのメソッド
+  def follow(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  #フォローを外すときのメソッド
+  def unfollow(user)
+    following_relationships.find_by(following_id: user.id).destroy
+  end
+
   attachment :profile_image
 
   def already_booked?(post)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_125723) do
+ActiveRecord::Schema.define(version: 2020_06_09_075312) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 2020_06_05_125723) do
     t.string "finish_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
