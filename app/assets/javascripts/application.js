ff455f902408e6fe34beb1d272770f4a733355f9
katayama623 <k6t2y3m@gmail.com>
2020-06-11 06:53:32 +0000

    [add] topページのデザインの雛形

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,3 +63,28 @@ $(document).on('turbolinks:load', function () {
     readURL(this);
   });
 });
+
+$(window).scroll(function() {
+ $(".animation .anm_mod").each(function() {
+  const position = $(this).offset().top;
+  const scroll = $(window).scrollTop();
+  const windowHeight = $(window).height();
+  if (scroll > position - windowHeight) {
+   $(this).addClass("active");
+  }
+  if (scroll < 100) {
+   $(this).removeClass("active");
+  }
+ });
+});
+
+$(function() {
+ $('a[href^="#"]').click(function() {
+  const speed = 800;
+  const href = $(this).attr("href");
+  const target = $(href == "#" || href == "" ? "html" : href);
+  const position = target.offset().top;
+  $("html, body").animate({ scrollTop: position }, speed, "swing");
+  return false;
+ });
+});
